cc562a5bf11c48f27400228bf744b0651f1e8ab7
Ryan Bogle <ryan@mediahound.com>
2014-10-21 16:38:46 -0700

    Removed Uglify compression.

--- Gulpfile.js
+++ Gulpfile.js
@@ -27,7 +27,31 @@ paths.buildCompiled = paths.build + '/compiled';
 var uglifyOpts = {
 
   mangle: false,
-  warnings: false
+  warnings: true,
+  compress: false
+  /*
+  compress:{
+    sequences     : true,  // join consecutive statemets with the “comma operator”
+    properties    : false,  // optimize property access: a["foo"] → a.foo
+    dead_code     : true,  // discard unreachable code
+    drop_debugger : true,  // discard “debugger” statements
+    unsafe        : false, // some unsafe optimizations (see below)
+    conditionals  : false,  // optimize if-s and conditional expressions
+    comparisons   : false,  // optimize comparisons
+    evaluate      : true,  // evaluate constant expressions
+    booleans      : true,  // optimize boolean expressions
+    loops         : false,  // optimize loops
+    unused        : false,  // drop unused variables/functions
+    hoist_funs    : false,  // hoist function declarations
+    hoist_vars    : false, // hoist variable declarations
+    if_return     : false,  // optimize if-s followed by return/continue
+    join_vars     : true,  // join var declarations
+    cascade       : true,  // try to cascade `right` into `left` in sequences
+    side_effects  : true,  // drop side-effect-free statements
+    warnings      : true  // warn about potentially dangerous optimizations/code
+    //global_defs   : {}     // global definitions
+  }
+  */
 
 };
 
