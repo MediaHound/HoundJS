35a3d951074e4fb9d39a9450b00b8e2252100c4f
Dustin Bachrach <ahdustin@gmail.com>
2015-08-24 10:15:51 -0700

    Clean up gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,14 @@
-var gulp        = require('gulp'),
-    jshint      = require('gulp-jshint'),
-    concat      = require('gulp-concat'),
-    replace     = require('gulp-replace'),
-    uglify      = require('gulp-uglify'),
-    rename      = require('gulp-rename'),
-    jasmine     = require('gulp-jasmine'),
-    runSequence = require('run-sequence'),
-    execFile    = require('child_process').execFile;
+var gulp        = require('gulp');
+var jshint      = require('gulp-jshint');
+var concat      = require('gulp-concat');
+var replace     = require('gulp-replace');
+var uglify      = require('gulp-uglify');
+var rename      = require('gulp-rename');
+var jasmine     = require('gulp-jasmine');
+var runSequence = require('run-sequence');
+var execFile    = require('child_process').execFile;
 
-var libName = 'hound'
+var libName = 'hound';
 
 // Paths for tasks
 var paths = {
@@ -58,7 +58,7 @@ var execFileHelper = function(command, args, callback) {
 // Lints Javascript code for errors
 gulp.task('lint', function() {
   return gulp.src(paths.src.js)
-    .pipe(jshint('./.jshintrc'))
+    .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
@@ -104,6 +104,7 @@ gulp.task('test', function(done) {
 gulp.task('default', function(done) {
   runSequence(
     'lint',
-    'dist'
+    'dist',
+    done
   )
 });
