c8e15b42a753b1a98b20eda15acbfa2376ec86fc
Dustin Bachrach <dustin@Dustins-MacBook-Pro.local>
2017-01-16 14:43:47 -0800

    Remove console log

--- v2/src/action/action.js
+++ v2/src/action/action.js
@@ -1,8 +1,6 @@
 import houndRequest from '../request/hound-request.js';
 
 export default ({ ids, action }) => {
-  console.log(`Bulk take action (${action}) on ${ids}`);
-
   return houndRequest({
     method: 'PUT',
     endpoint: `graph/action/${action}`,
