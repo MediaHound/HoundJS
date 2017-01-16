a68d3c335575753973c33c418761200bc937af4c
Dustin Bachrach <dustin@Dustins-MacBook-Pro.local>
2017-01-16 15:21:40 -0800

    Search passes version 1.3 parameter

--- v2/src/query/search/search.js
+++ v2/src/query/search/search.js
@@ -6,6 +6,7 @@ export const all = ({ searchTerm, scopes, pageSize }) => {
     method  : 'GET',
     endpoint: `search/all/${extraEncode(searchTerm)}`,
     params: {
+      version: '1.3', // TODO: Remove when this is implicit
       pageSize,
       types: Array.isArray(scopes) ? scopes : undefined
     },
@@ -18,6 +19,7 @@ export const segmented = ({ searchTerm, scopes, siloPageSize, includeAll = true 
     method  : 'GET',
     endpoint: `search/segmented/${extraEncode(searchTerm)}`,
     params: {
+      version: '1.3', // TODO: Remove when this is implicit
       siloPageSize,
       includeAll,
       types: Array.isArray(scopes) ? scopes : undefined
