d420b6d559ce956b02d583b1e8d5bbef1828e523
Dustin Bachrach <ahdustin@gmail.com>
2017-10-19 13:56:04 -0700

    Silos take sharedPatterns

--- v2/src/query/silo/silo.js
+++ v2/src/query/silo/silo.js
@@ -1,11 +1,12 @@
 import houndRequest from '../../request/hound-request.js';
 
-export default ({ pattern, global, silosPerPage, itemsPerSilo, debug = false }) => {
+export default ({ patterns, sharedPatterns, global, silosPerPage, itemsPerSilo, debug = false }) => {
   return houndRequest({
     method: 'GET',
     endpoint: 'graph/silo',
-    params: { pattern, global, silosPerPage, itemsPerSilo },
+    params: { patterns, sharedPatterns, global, silosPerPage, itemsPerSilo },
     responseType: 'silo',
-    debug
+    debug,
+    paramsProper: true
   });
 };
